0ebaf77f8a8fa3d5456da671b649e36b24173376
amor <adrien_morvan@hotmail.fr>
2019-03-31 16:49:54 +0200

    better event handling

--- app/controllers/EventController.scala
+++ app/controllers/EventController.scala
@@ -8,7 +8,7 @@ import play.api.data._
 import play.api.data.Forms._
 import models.objects.{Event}
 import play.api.mvc.MessagesActionBuilder
-
+import play.api.mvc.Results._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
@@ -17,11 +17,13 @@ class EventController @javax.inject.Inject() (override val app: Application, mes
   extends AuthController("home") with play.api.i18n.I18nSupport {
   ApplicationDatabase.migrateSafe()
 
-  var events = List[Event]()
+  var events = Map[Long, Event](0.toLong -> Event(0.toLong, "My first event", "Cool stuff", "Amicale scolaire",
+    List("Entraide"), "Entraide"), 1.toLong -> Event(1, "My first event", "Cool stuff", "Amicale scolaire", List("Entraide"), "Entraide"))
+  var eventCounter = 2.toLong
 
   val eventForm = Form(
     mapping(
-      "id" -> ignored(1234.toLong),
+      "id" -> ignored(eventCounter.toLong),
       "title" -> nonEmptyText,
       "description" -> nonEmptyText,
       "groupName" -> text,
@@ -30,18 +32,23 @@ class EventController @javax.inject.Inject() (override val app: Application, mes
     )(Event.apply)(Event.unapply)
   )
 
-  def listing() = withoutSession("") { implicit request => implicit td =>
-    Future.successful(Ok(views.html.category(events)))
+  def listings() = withoutSession("") { implicit request => implicit td =>
+    Future.successful(Ok(views.html.category(events.values.toList)))
+  }
+
+  def listing(userId: String) = withoutSession("") { implicit request => implicit td =>
+    Future.successful(Ok(views.html.category(List(events { userId.toLong }))))
   }
 
   def form() = withoutSession("") { implicit request => implicit td =>
+    eventCounter += 1
     Future.successful(Ok(views.html.form(eventForm)))
   }
 
   def submit = Action { implicit request =>
     val event = eventForm.bindFromRequest.get
-    events = events :+ event
-    Ok("Hi %s".format(event))
+    events = events + (eventCounter -> event)
+    Redirect(s"event/${eventCounter.toString}")
   }
 
 }
